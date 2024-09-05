4b11136777ee0469b37715eac4a6b56345654ce2
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-05 17:57:11 +0400

    task

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-import random
-print(''.join(chr(i) for i in range(97, 123) if chr(i) not in ('q', 'e')), end='')
+for i in range(97, 123):
+    if i != 101 and i != 113:
+print(chr(i), end='')
