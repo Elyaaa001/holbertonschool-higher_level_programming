77e17e8a85b6e44183ec3ed23553be0aaea97640
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-08 19:46:35 +0000

    fixx

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,5 +6,5 @@ def uppercase(s):
             result += chr(ord(i) - 32)
         else:
             result += i
-    print("Converted string: {}".format(result))
+    print("{}".format(result))
 print('')
