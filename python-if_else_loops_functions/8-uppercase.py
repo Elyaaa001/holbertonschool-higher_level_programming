dd54184400f2cae714db4efa45782ad3955ff17a
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-08 20:45:33 +0000

    fixx

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def uppercase(s):
-    result = ""
-    for i in s:
-        if 'a' <= i <= 'z':
-            result += chr(ord(i) - 32)
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            num = 32
         else:
-            result += i
-    print("{}".format(result), end="")
-print('')
+            num = 0
+        print("{:c}".format(ord(str[i]) - num), end='')
+    print()
