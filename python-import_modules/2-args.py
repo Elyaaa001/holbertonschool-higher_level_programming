e21323c3aafc886eb53b0168a3e8ac41ccd37d9c
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-09 19:25:43 +0000

    errors

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-argv = sys.argv
-argv = []
-x = len(argv) - 1
-if x == 0:
-    print("0 arguments.")
-elif x == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(x))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
