83a0385cca773d1b94ac43854aa6fa15a6f58301
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-09 18:58:58 +0000

    task

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+argv = []
+len(argv) == x
+if x == 0:
+    print("{} arguments.".format(x))
+    if x > 0 :
+        print("{} arguments:".format(x))
+         for i in range(1, len(argv)):
+            print(f"{i}: {arguments[i]}")`
