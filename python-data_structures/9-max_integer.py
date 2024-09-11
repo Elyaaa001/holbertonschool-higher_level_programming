daea554176ab83a68e9fe156c7b8ecd963dd57d2
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-11 15:06:22 +0000

    taskkk

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
+    largest = my_list[0]
     for i in my_list:
-        largest = my_list[0]
         if i > largest:
             largest = i
     return largest
