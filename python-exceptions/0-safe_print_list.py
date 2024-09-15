214d2da3200d3220d35f5e3f48ec6506f9f2c9b4
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-15 16:07:43 +0000

    task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         while count < x:
-            print({:d}".format(x))
+            print({:d}".format(x), end = '')
             count += 1
     except IndexError:
     print()
-
