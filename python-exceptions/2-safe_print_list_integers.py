dbfb38db357f80e71d01a3dd59b513ee02a317c2
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-15 16:40:26 +0000

    task1

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+    while count < x :
+        value = my_list[i]
+        if value is int:
+            print("{:d}".format(value), end='')
+            count += 1
+    except IndexError:
+    print()
