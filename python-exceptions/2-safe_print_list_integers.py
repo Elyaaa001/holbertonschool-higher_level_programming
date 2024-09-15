b11a201bb2eb52478c83d810cbb364889a1d8827
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-15 16:43:13 +0000

    task1

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         while count < x:
             value = my_list[i]
-        if value is int:
+        if isinstance(value, int):
             print("{:d}".format(value), end='')
             count += 1
     except IndexError:
