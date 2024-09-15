21d60a84b32a47a961a4e2a0f42d93b70445981c
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-15 17:04:22 +0000

    error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(0, x)
-    try:
-        print("{:d}".format(my_list[i]), end = "")
-    except IndexError:
+        try:
+            print("{:d}".format(my_list[i]), end = "")
+            ret += 1
+        except IndexError:
         pass
     print()
     return count
