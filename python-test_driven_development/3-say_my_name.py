132383e05abcaa0088f55ef3fb6883c121bdd057
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-17 07:42:56 +0000

    task

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,16 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    Print the full name by combining the first name and optional last name.
+
+    Args:
+        first_name (str): The first name to be printed.
+        last_name (str, optional): The last name to be printed. Default is "".
+
+    Raises:
+        TypeError: If either first_name or last_name is not a string.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
