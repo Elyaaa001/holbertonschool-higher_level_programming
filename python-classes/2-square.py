01ad5e6990cb4919b273a95c533387ea7144a575
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-20 15:52:02 +0000

    taskk

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,5 +5,9 @@
 class Square():
     """Define class"""
     def __init__(self, size):
-        self._size = size
+        if not isinstance (size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
     pass
