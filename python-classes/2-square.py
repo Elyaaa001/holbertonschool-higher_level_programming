81841a4c9021ecad15e651def87f492218d08201
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-20 17:57:50 +0000

    error againnn

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""Module for creating a class that defines a square."""
+"""Define a class Square."""
 
 
 class Square:
-    """Class that defines a square with a given size."""
-    
-    def __init__(self, size):
-        """Initialize the square with a given size.
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
 
         Args:
-            size (int): The size of the square's side.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be > 0")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
