2267bc185ac3541dea04e748ad6df145eb5cfc3e
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-21 11:00:02 +0000

    error

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
+
+"""This module defines a class Square."""
+
+
 class Square:
+    """
+    An class that defines a square.
+    """
     def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
-    # Property
+    def area(self):
+        return self.__size * self.__size
+
     @property
     def size(self):
         return self.__size
 
-    # Setter modifies
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
-
-    def area(self):
-        return self.__size ** 2
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def my_print(self):
-        size = self.__size
-
-        if size == 0:
-            print()
-
-        for row in range(size):
-            print('#' * size)
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                print("#" * self.__size)
