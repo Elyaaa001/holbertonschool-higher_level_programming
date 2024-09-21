2020c21fe754c40695f7c153c0a46dc28bea1113
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-21 11:22:27 +0000

    error

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 """This module defines a class Square."""
 
 
@@ -28,6 +26,20 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
