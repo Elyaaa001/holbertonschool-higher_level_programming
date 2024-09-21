8b7165616ebef7415dc9e5b148d4731574772bc3
Elyaaa001 ha <gabibovaelina75@gmail.com>
2024-09-21 11:20:47 +0000

    error

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -28,7 +28,23 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
 
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
     def my_print(self):
         if self.__size == 0:
             print("")
