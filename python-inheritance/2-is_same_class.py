c5dc50c5043b494433a17839b514516110bbf7af
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-25 18:38:24 +0400

    pep

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """same class"""
+
+
 def is_same_class(obj, a_class):
     """checks if an object is instance of givem class"""
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     return False
