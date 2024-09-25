8c9c0eeab03dccfbc39f6c9e326574432ba0de2b
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-25 18:49:12 +0400

    pep againnnnnnnnnn

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,4 @@
 
 def inherits_from(obj, a_class):
     """body"""
-    if isinstance(obj, a_class) and type(obj) is a_class:
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class) and type(obj) != a_class
