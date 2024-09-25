1c90e0c5c03d7503b39f43dd12a17ba6daae6f58
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-25 18:45:02 +0400

    task

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""checks object"""
+
+
+def inherits_from(obj, a_class):
+    """body"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
