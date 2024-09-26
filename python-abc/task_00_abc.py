5e9b0a2d4c0e5603010c59ba5121baa10e4e2860
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-26 15:15:20 +0400

    error

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 from abc import ABC, abstractmethod
+
 class Animal(ABC):
+    """Abstract base class for animals."""
+    
     @abstractmethod
     def sound(self):
-        
+        """Return sound"""
+        pass
+
 class Dog(Animal):
+    """ dog"""
     
     def sound(self):
         return "Bark"
 
 class Cat(Animal):
+    """Represents a cat"""
     
     def sound(self):
         return "Meow"
+
+if __name__ == "__main__":
+    dog = Dog()
+    cat = Cat()
+
+    print(f"The dog says: {dog.sound()}")
+    print(f"The cat says: {cat.sound()}")
