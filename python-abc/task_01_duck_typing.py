158d2171fe0da5f9fe101e15c9c92402a3b83a7e
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-26 17:31:24 +0400

    taskkkkk

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,57 +1,42 @@
 #!/usr/bin/env python3
-"""create a class"""
-
 from abc import ABC, abstractmethod
+import math
 
 
 class Shape(ABC):
-    """Shape Class abstract"""
     @abstractmethod
-    
     def area(self):
-        """abstract method area"""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """abstract method perimeter"""
         pass
 
-
 class Circle(Shape):
-    """Circle class that inherits from Shape"""
 
     def __init__(self, radius):
-        """initialize Circle"""
         self.radius = radius
 
     def area(self):
-        """implement area method"""
-        return self.radius ** 2 * 3.14159265359
+        return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """implement perimeter method"""
-        return 2 * (3.14159265359 * self.radius)
+        return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """Rectangle class that inherits from Shape"""
 
     def __init__(self, width, height):
-        """initialize Rectangle"""
         self.width = width
         self.height = height
 
     def area(self):
-        """implement area method"""
         return self.width * self.height
 
     def perimeter(self):
-        """implement perimeter method"""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """print the area and perimeter of the shape"""
-    print("Area: {}".format(shape.area()))
-    print("Perimeter: {}".format(shape.perimeter()))
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
