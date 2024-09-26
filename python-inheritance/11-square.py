35e2a2f50f8f90a9f6a644a6f7c49196599e087b
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-26 12:03:31 +0400

    task

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -12,4 +12,7 @@ class Square(Rectangle):
 
     def area(self):
         return self.__size * self.__size
-
+    
+    
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
