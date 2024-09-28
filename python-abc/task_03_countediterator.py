333708beac429eafe3138f1545acc82fca274e99
Elyaaa001 <gabibovaelina75@gmail.com>
2024-09-28 19:38:20 +0400

    tskkk

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -2,21 +2,18 @@
 
 
 class CountedIterator:
-    """Class named CountedIterator"""
+
     def __init__(self, iterable):
-        """initialized iterable"""
         self.iterator = iter(iterable)
         self.count = 0
 
     def __next__(self):
-        """defines next iterator"""
         try:
             item = next(self.iterator)
             self.count += 1
             return item
         except StopIteration:
-            raise StopIteration("No more items to iterate")
+            raise StopIteration
 
     def get_count(self):
-        """defines get count"""
         return self.count
