16c2b608e903a0754753941ea1cfec63fcc49b93
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-30 13:31:23 +0400

    style

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that reads a file and prints its content."""
 
+
 def read_file(filename=""):
     """Function that reads a UTF-8 text file and prints its content."""
     with open(filename, "r", encoding="utf-8") as file:
