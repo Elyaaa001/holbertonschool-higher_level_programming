f069cbae6b045a22d16d650800ba4ed3cc648e16
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-30 13:43:29 +0400

    pep

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a UTF-8 text file and returns the number of characters added."""
+    """appends a string"""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
