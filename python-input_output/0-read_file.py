467712382ecf1675217b7c9a26dc07357ec208c0
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-30 13:30:27 +0400

    error

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""module that reads file"""
-def read_file(filename=""):
-"""func"""
+"""Module that reads a file and prints its content."""
 
-with open("filename.txt", "r") as file:
-    content = file.read()
+def read_file(filename=""):
+    """Function that reads a UTF-8 text file and prints its content."""
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content, end="")
