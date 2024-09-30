008d6910c441a6f37ff6ec630c82d7133a45635a
Elyaaa001 <gabibovelina75@gmail.com>
2024-09-30 13:40:39 +0400

    pep

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """module"""
+
+
+
 def write_file(filename="", text=""):
     """writes a string to a UTF-8 text file"""
     with open(filename, "w", encoding="utf-8") as file:
