9249628f4aee9f861220a6f8d82401d3ff8fb8b1
Elyaaa001 <gabibovaelina75@gmail.com>
2024-10-03 14:41:42 +0400

    task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module documentation"""
+
+def load_from_json_file(filename):
+    """function that creates a object from a json file"""
+    import json
+    with open('data.json', 'r') as file:
+        json_object = json.load(file)
+        
