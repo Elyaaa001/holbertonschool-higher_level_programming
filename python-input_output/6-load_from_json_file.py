231e1d5c772eda9e8e7fa2288126c7c8c3c84d7d
Elyaaa001 <gabibovelina75@gmail.com>
2024-10-04 18:17:37 +0400

    error

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """module documentation"""
 
+import json
+
 def load_from_json_file(filename):
-    """function that creates a object from a json file"""
-    import json
+    """function that creates an object from a JSON file"""
+   
+   
     with open(filename, 'r') as file:
-        json_object = json.load(file)
         return json.load(file)
-        
