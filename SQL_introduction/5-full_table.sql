7ca10cd154584e4aeb310a0076c96ce4a8836c0d
Elyaaa001 <gabibovelina75@gmail.com>
2024-10-22 13:27:19 +0400

    error

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- desription
-SELECT COLUMN_NAME, DATA_TYPE, COLUMN_TYPE 
-FROM information_schema.COLUMNS 
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' 
-  AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
