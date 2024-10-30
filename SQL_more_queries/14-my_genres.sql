4e221c119c66614fea8d9c232ec38627a011fbd6
Elyaaa001 <gabibovelina75@gmail.com>
2024-10-30 09:25:03 +0400

    errorrrrrr

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- import dump 
-SELECT genre.name AS name
-FROM genre
-JOIN tv_show_genre ON genre.id = tv_show_genre.genre_id
-JOIN tv_shows ON tv_show_genre.tv_show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY genre.name ASC;
+SELECT tv_genres.name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
